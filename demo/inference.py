cecd9c136afcb438b217f70c1a7f10015a16543e
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2019-09-17 23:40:43 +0200

    Update inference.py

--- demo/inference.py
+++ demo/inference.py
@@ -39,7 +39,7 @@ class PlaneClustering(Masker):
             if torch.max(mask) <= 0.5:
                 raise ValueError('No apex')
             src_points = torch.cat([self.assist_info, mask[..., None]], dim=2)
-            pos_points = src_points[(mask > 0.5) & (mask < 0.8)]
+            pos_points = src_points[(mask > 0.1) & (mask < 0.8)]
             planes = plane_init(pos_points, ideal_value=1)
             planes = plane_clustering(pos_points, planes)
             points = get_intersection_of_plane(planes)
