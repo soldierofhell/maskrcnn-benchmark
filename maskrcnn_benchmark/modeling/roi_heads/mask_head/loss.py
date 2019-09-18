2959cdd5c8ab56dfef6d1e9ea86548fb3c33585c
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2019-09-18 10:39:01 +0200

    Update loss.py

--- maskrcnn_benchmark/modeling/roi_heads/mask_head/loss.py
+++ maskrcnn_benchmark/modeling/roi_heads/mask_head/loss.py
@@ -142,7 +142,10 @@ class MaskRCNNLossComputation(object):
         input = torch.where(target>0, input, torch.zeros_like(input))
         
         input_1 = input[target>0]
-        print('input min, max, mean, std: ', torch.min(input_1), torch.max(input_1), torch.mean(input_1), torch.std(input_1))
+        target_1 = input[target>0]
+        
+        print('input min, max, mean, std: ', torch.cat(torch.min(input_1), torch.max(input_1), torch.mean(input_1), torch.std(input_1)))
+        print('input min, max, mean, std: ', torch.cat(torch.min(target_1), torch.max(target_1), torch.mean(target_1), torch.std(target_1)))
         
         #l1_loss = L1Loss()
         #mask_loss = 5*l1_loss(input, target)
