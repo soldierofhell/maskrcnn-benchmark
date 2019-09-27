3e3bf5f2efcc8072304c56355b44a9add98e8f7a
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2019-09-27 11:51:50 +0200

    Update __init__.py

--- maskrcnn_benchmark/data/datasets/__init__.py
+++ maskrcnn_benchmark/data/datasets/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-from .coco import COCODataset
+#from .coco import COCODataset
 #from .coco_carfusion import COCODataset
+from .coco_text import COCODataset
 from .voc import PascalVOCDataset
 from .concat_dataset import ConcatDataset
 
